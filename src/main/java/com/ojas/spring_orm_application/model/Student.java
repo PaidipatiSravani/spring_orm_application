c3286377725f63a571f429b9cd6eea1304c31cc5
Supriya Vahini Madipalli <vahinikalaskar@gmail.com>
2023-01-05 17:47:14 +0530

    modification_1

--- src/main/java/com/ojas/spring_orm_application/model/Student.java
+++ src/main/java/com/ojas/spring_orm_application/model/Student.java
@@ -1,5 +1,35 @@
 package com.ojas.spring_orm_application.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name="Student")
 public class Student {
-
+	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int studentId;
+	private String studentName;
+	private String studentAddress;
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentAddress() {
+		return studentAddress;
+	}
+	public void setStudentAddress(String studentAddress) {
+		this.studentAddress = studentAddress;
+	}
+	
 }
--- src/main/java/com/ojas/spring_orm_application/model/Users.java
+++ src/main/java/com/ojas/spring_orm_application/model/Users.java
@@ -1,5 +1,43 @@
 package com.ojas.spring_orm_application.model;
 
-public class Users {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="users")
+public class Users {
+	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int uid;
+	private String username;
+	private String password;
+	private String type;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
 }
